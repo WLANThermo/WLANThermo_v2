6526b02e0f788e46f830b8fe24aa52df410a937d
BjoernSch <mail@bjoern-schrader.de>
2016-02-20 21:03:11 +0100

    Anpassung Nextion-Update

--- software/var/www/control/update.php
+++ software/var/www/control/update.php
@@ -15,7 +15,7 @@
 		echo '</div>';
 		exit;
 	}
-	if ((!isset($_SESSION["newversion"])) or (!isset($_SESSION["nextionupdate"]))){
+	if ((!isset($_SESSION["newversion"])) and (!isset($_SESSION["nextionupdate"]))){
 		echo '<div id="info_site">';
 		echo "  <head> <meta http-equiv=\"refresh\" content=\"1;URL='../index.php'\"> </head> <body> <h2>keine neuen Updates vorhanden...</h2></body>";
 		echo '</div>';
@@ -248,7 +248,7 @@ if(isset($_POST["back"])) {
 			<h1>Update Installieren</h1>
 			<br>
 			<?php
-				exec("/usr/sbin/wlt_2_updatenextion.sh /usr/share/WLANThermo/nextion/NX3224T028.tft > /var/www/tmp/error.txt &",$output);
+				exec("sudo /usr/sbin/wlt_2_updatenextion.sh /usr/share/WLANThermo/nextion/NX3224T028.tft > /var/www/tmp/error.txt &",$output);
 				echo "  <head> <meta http-equiv=\"refresh\" content=\"1;URL='../index.php'\"> </head> <body> <h2>Das Update wird nun Installiert...</h2></body>";
 			?>
 		</div>
