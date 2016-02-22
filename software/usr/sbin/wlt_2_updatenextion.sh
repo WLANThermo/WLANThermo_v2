29132d49eccaa2a0f1e475685fedf597d9642913
BjoernSch <mail@bjoern-schrader.de>
2016-02-22 10:48:14 +0100

    Nextion-Service zuverlässiger stoppen
    WD-Dienst wird beendet, da dieser der Parent-Prozess vom Display-Dienst
    ist.
    Nextion-Prozess wird per kill beendet.

--- software/usr/sbin/wlt_2_updatenextion.sh
+++ software/usr/sbin/wlt_2_updatenextion.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 echo 'Displaydienst wird beendet' > /var/www/tmp/nextionupdatelog
-service WLANThermoNEXTION stop
-sleep 5
+service WLANThermoWD stop
+if [ -e '/var/run/wlt_2_nextion.pid' ]
+then
+   kill $(< /var/run/wlt_2_nextion.pid)
+   sleep 5
+fi
 python /usr/sbin/nextionupload.py $1 >> /var/www/tmp/nextionupdatelog
 RETVAL=$?
 [ $RETVAL -eq 0 ] && echo 'Erfolgreich upgedatet!' && rm /var/www/tmp/nextionupdate
@@ -11,5 +15,4 @@ sleep 1
 echo 'WLANThermodienste werden neu gestartet' >> /var/www/tmp/nextionupdatelog
 service WLANThermo restart
 service WLANThermoWD restart
-service WLANThermoNEXTION restart
 mv /var/www/tmp/nextionupdatelog /var/www/tmp/nextionupdatelog.old
