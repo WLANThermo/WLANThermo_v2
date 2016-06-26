cf495c75efff4b7c6d33716773e9ed72052d6637
BjoernSch <BjoernSch@users.noreply.github.com>
2016-06-26 21:19:41 +0200

    Version auf 2.4.0-0rc1 geändert

--- software/var/www/header.php
+++ software/var/www/header.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
-$_SESSION["webGUIversion"] = "V2.4.0-0";
+$_SESSION["webGUIversion"] = "V2.4.0-0rc1";
 $title = "WLAN Thermometer";
 $document_root = getenv('DOCUMENT_ROOT');
 ?>
