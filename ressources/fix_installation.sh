494b34b4c08310b74c298824dea3b97a5ba62780
BjoernSch <mail@bjoern-schrader.de>
2016-12-14 20:17:21 +0100

    Update version

--- ressources/fix_installation.sh
+++ ressources/fix_installation.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 dpkg --configure -a
-wget https://github.com/WLANThermo/WLANThermo_v2/releases/download/v2.5.0/WLANThermo_install-2.5.0-0.run
+wget https://github.com/WLANThermo/WLANThermo_v2/releases/download/v2.5.2/WLANThermo_install-2.5.2-0.run
 
-lines=`grep --max-count 1 --line-regexp --line-number --text '# ---- END OF SCRIPT - DON´T CHANGE THIS LINE ----' WLANThermo_install-2.5.0-0.run | cut -d: -f 1`
+lines=`grep --max-count 1 --line-regexp --line-number --text '# ---- END OF SCRIPT - DON´T CHANGE THIS LINE ----' WLANThermo_install-2.5.2-0.run | cut -d: -f 1`
 startline=$((lines + 1))
 program=wlanthermo
 
 echo "Extract the package"
-tail -n +$startline WLANThermo_install-2.5.0-0.run > /tmp/${program}.deb
-rm WLANThermo_install-2.5.0-0.run
+tail -n +$startline WLANThermo_install-2.5.2-0.run > /tmp/${program}.deb
+rm WLANThermo_install-2.5.2-0.run
 sleep 2
 
 echo 'install /tmp/${program}.deb'
