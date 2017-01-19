4ef1a71875033f4618a632543d818f3a1d6427e6
BjoernSch <mail@bjoern-schrader.de>
2017-01-19 23:02:51 +0100

    Update version string

--- software/usr/sbin/wlt_2_nextion.py
+++ software/usr/sbin/wlt_2_nextion.py
@@ -960,7 +960,7 @@ def NX_display():
     global temps_event, channels_event, pitmaster_event, pitmasterconfig_event
     global Config
     
-    nextion_versions = ['v2.1']
+    nextion_versions = ['v2.2']
     
     # Version des Displays prüfen
     display_version = str(NX_getvalue('main.version.txt'))
