ad416a2d93eef23b00d88bf611444e4117eee7d4
BjoernSch <mail@bjoern-schrader.de>
2017-02-16 14:17:50 +0100

    Improving install script

--- build/installer/installer.sh
+++ build/installer/installer.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 set -e
 
+echo "Starting WLANThermo installation"
+echo "----------------------------------------------------------"
+
 if [ `whoami` != root ]; then
     echo "Please run this script as root or using sudo!"
     exit
 fi
-
+  
 grep 'VERSION_ID="8"' /etc/os-release &> /dev/null
 if [ $? -ne 0 ]; then
   echo "This installer need Raspbian Jessie!"
   exit
 fi
+
+#
+# Stop processes to speed up installation
+#
+
+echo "Stopping WLANThermo processes"
+echo "----------------------------------------------------------"
+systemctl stop WLANThermo.service
+systemctl stop pigpiod.service
+pkill -f wlt_2_nextion.py
+pkill gnuplot
 
 lines=`grep --max-count 1 --line-regexp --line-number --text '# ---- END OF SCRIPT - DON´T CHANGE THIS LINE ----' $0 | cut -d: -f 1`
 startline=$((lines + 1))
@@ -26,14 +40,17 @@ else
 fi
 
 echo "Disable shell and kernel messages on the serial connection"
+echo "----------------------------------------------------------"
 if [ $SYSTEMD -eq 0 ]; then
   sed -i /etc/inittab -e "s|^.*:.*:respawn:.*ttyAMA0|#&|"
 fi
 sed -i /boot/cmdline.txt -e "s/console=ttyAMA0,[0-9]\+ //"
+sed -i /boot/cmdline.txt -e "s/console=serial0,[0-9]\+ //"
 
 program=wlanthermo
 
 echo "Check Ramdrive and create it if it doesn't exist"
+echo "----------------------------------------------------------"
 RD=$(cat /etc/fstab|grep /var/www/tmp|grep -v grep|wc -l)
 
 if  [ $RD == 0 ]; then
@@ -46,26 +63,42 @@ if  [ $RD != 0 ]; then
   echo -e "[\033[42m\033[30m OK \033[0m] RAM Drive already exists"
 fi
 
+echo "Updating System:"
+echo "----------------------------------------------------------"
+apt-get update
+apt-get upgrade -y
+
 echo "Install depencies:"
-sudo apt-get update
+echo "----------------------------------------------------------"
 aptitude --safe-resolver -y install build-essential sudo vim htop iftop iotop gnuplot-nox lighttpd apache2-utils php5-cgi php5-gd php5-intl php5-curl fswebcam imagemagick pigpio python python-dev python-serial python-psutil python-pyinotify python-urllib3 python3 python3-dev python3-serial ntpstat python-pip python3-pip
 aptitude --safe-resolver -y remove python-pigpio python3-pigpio
 
 echo "Install additional Python packages"
+echo "----------------------------------------------------------"
 pip install bitstring
 
 echo "Updating pigpiod"
+echo "----------------------------------------------------------"
 rm -f /tmp/pigpio.tar
 rm -rf /tmp/PIGPIO
 pushd /tmp
 wget abyz.co.uk/rpi/pigpio/pigpio.tar
 tar xf pigpio.tar
 cd PIGPIO
+
+echo "Removing old pigpiod:"
+echo "----------------------------------------------------------"
+make uninstall
+make prefix=/usr uninstall
+
+echo "Installing new pigpiod, this may take some time!"
+echo "----------------------------------------------------------"
 make -j4
 make prefix=/usr install
 popd
 
 echo "Extract the package"
+echo "----------------------------------------------------------"
 tail -n +$startline $0 > /tmp/${program}.deb
 sleep 2
 echo 'install /tmp/${program}.deb'
@@ -73,7 +106,12 @@ dpkg -i /tmp/${program}.deb
 
 url=$(cat /etc/hostname)
 echo " "
-echo "========***********WLANThermo installed completely open http://$url in a browser! ***********==========="
+echo "===================================================================================================="
+echo "========*********************************************************************************==========="
+echo "========***********    WLANThermo installed completely! Please reboot now!    ***********==========="
+echo "========***********         Open http://$url in a browser afterwards!         ***********==========="
+echo "========*********************************************************************************==========="
+echo "===================================================================================================="
 echo " "
 
 exit 0
