6f6e4b8cf5c886e9aee8e5f2973378a9b4574b36
Björn Schrader <mail@bjoern-schrader.de>
2018-05-05 20:09:25 +0200

    Write log to update to block GUI
    Use /var/www/tmp/update instead of /var/www/tmp/update.log

--- software/usr/bin/wlt_2_update_system.sh
+++ software/usr/bin/wlt_2_update_system.sh
@@ -8,28 +8,33 @@ then
     fi
 fi
 
-echo "Fixing broken installations..." > /var/www/tmp/update.log
-apt -y install --fix-broken &>> /var/www/tmp/update.log
-echo "Updating package list..." >> /var/www/tmp/update.log
-apt update &>> /var/www/tmp/update.log
+echo "Fixing broken installations..." > /var/www/tmp/update
+apt -y install --fix-broken &>> /var/www/tmp/update
+echo "Updating package list..." >> /var/www/tmp/update
+apt update &>> /var/www/tmp/update
 
 if [[ -z "$full_upgrade" ]]
 then
-    echo "Hold back wlanthermo..." >> /var/www/tmp/update.log
-    apt-mark hold wlanthermo &>> /var/www/tmp/update.log
+    echo "Hold back wlanthermo..." >> /var/www/tmp/update
+    apt-mark hold wlanthermo &>> /var/www/tmp/update
 fi
 
 # Do full upgrade
-apt -y dist-upgrade &>> /var/www/tmp/update.log
+apt -y dist-upgrade &>> /var/www/tmp/update
 
 if [[ -z "$full_upgrade" ]]
 then
-    echo "Unhold wlanthermo..." >> /var/www/tmp/update.log
-    apt-mark unhold wlanthermo &>> /var/www/tmp/update.log
+    echo "Unhold wlanthermo..." >> /var/www/tmp/update
+    apt-mark unhold wlanthermo &>> /var/www/tmp/update
 fi
 
 # Cleanup afterwards
-echo "Cleaning up..." >> /var/www/tmp/update.log
-apt -y autoremove &>> /var/www/tmp/update.log
-apt -y clean &>> /var/www/tmp/update.log
-mv /var/www/tmp/update.log /var/log/WLAN_Thermo/update.log
+echo "Cleaning up..." >> /var/www/tmp/update
+apt -y autoremove &>> /var/www/tmp/update
+apt -y clean &>> /var/www/tmp/update
+
+# Update available updates
+/etc/cron.daily/wlanthermo_updatecheck
+
+# Move log (and clear GUI)
+mv /var/www/tmp/update /var/log/WLAN_Thermo/update-$(date +%Y%M%d).log
