e58210cac16dd42162b0bcaab00760240e17c12a
Björn Schrader <mail@bjoern-schrader.de>
2018-05-26 22:12:27 +0200

    Change version number handling
    Remove Debian package version from shown string

--- build/create_installer.sh
+++ build/create_installer.sh
@@ -10,20 +10,26 @@ echo Kopiere Changelog in das Buildverzeichniss...
 cp -r ../changelog ./build/DEBIAN/changelog
 
 echo Suche Versionsnummer...
-VERSION=`dpkg-parsechangelog -lbuild/DEBIAN/changelog --show-field Version`
-echo Paketversion: $VERSION
+PKG_VERSION=`dpkg-parsechangelog -lbuild/DEBIAN/changelog --show-field Version`
+echo Paketversion: $PKG_VERSION
 echo Setze Version in den Paketdaten...
-echo Version: $VERSION >> ./build/DEBIAN/control
+echo Version: $PKG_VERSION >> ./build/DEBIAN/control
+
+[[ $PKG_VERSION =~ ^(.*)(-[[:digit:]]+)$ ]]
+VERSION=${BASH_REMATCH[1]}
 echo Setze Version in der header.php...
 sed -i -e "s/XXX_VERSION_XXX/V${VERSION}/g" ./build/var/www/header.php
+echo Setze Version in der wlt_2_nextion.py...
 sed -i -e "s/XXX_VERSION_XXX/V${VERSION}/g" ./build/usr/sbin/wlt_2_nextion.py
 
+PKG_FILE=./package/wlanthermo-$PKG_VERSION.deb
+INSTALLER=./run/WLANThermo_install-$PKG_VERSION.run
 echo Baue Paket...
-dpkg -b ./build ./package/wlanthermo-$VERSION.deb
+dpkg -b ./build $PKG_FILE
 
 echo Baue Installer...
-cat ./installer/installer.sh ./package/wlanthermo-$VERSION.deb > ./run/WLANThermo_install-$VERSION.run
-chmod +x ./run/WLANThermo_install-$VERSION.run
+cat ./installer/installer.sh $PKG_FILE > $INSTALLER
+chmod +x $INSTALLER
 
 echo Lösche Buildverzeichniss...
 rm -r ./build/*
